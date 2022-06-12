11284614e638e90a590a42edde93e71ea95a7128
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-12 10:28:53 -0300

    Add files via upload

---
+++ ProjetoTransportadora/src/Connection/Classes/Armazem.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connection.Classes;
+
+/**
+ *
+ * @author Bruno-PC
+ */
+public class Armazem {
+    private int id;
+    private String endereco;
+    private int nCaminhoes;
+    private double capacidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getnCaminhoes() {
+        return nCaminhoes;
+    }
+
+    public void setnCaminhoes(int nCaminhoes) {
+        this.nCaminhoes = nCaminhoes;
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+}
---
+++ ProjetoTransportadora/src/Connection/Metodos/MetodosArmazem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connection.Metodos;
+
+import Connection.Classes.Armazem;
+
+/**
+ *
+ * @author Bruno-PC
+ */
+public class MetodosArmazem {
+
+    public void salvarDados(Armazem a) {
+        
+    }
+    
+    
+}
