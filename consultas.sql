2e3261fc27eed156f5476469e5b5a2b6538910a4
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-07-01 08:31:11 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -6,15 +6,13 @@ select * from deposito;
 
 select * from viagem_deposito;
 
-insert into viagem (numero_viagem, peso, volume, fk_caminhoes) values(123, 112, 20, 1);
 
-insert into viagem_deposito (fk_viagem, fk_deposito) values(1, 2);
-
-insert into deposito (capacidade, endereco) values (225, 'Rua Xerxenhe, 77, Bairro Pripri');
+/*Consulta para saber as viagens de um caminhão, usando ID de caminhão
+como parâmetro para a pesquisa*/
 
 select c.id_caminhao, c.cod_licenca, v.numero_viagem, v.peso, d.endereco 
 from caminhoes as c 
 inner join viagem as v on v.fk_caminhoes = c.id_caminhao 
 inner join viagem_deposito as vd on vd.fk_viagem = v.id_viagem 
 inner join deposito as d on d.id_deposito = vd.fk_deposito
-where v.id_viagem = 1
+where v.id_viagem = 1;
